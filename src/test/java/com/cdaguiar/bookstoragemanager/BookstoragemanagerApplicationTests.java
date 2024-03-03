cb15fff9129cddb31c4f4bfcb8d64607fe41f0e1
Claudionei de Aguiar <cdaguiar23@gmail.com>
2024-03-03 14:21:43 -0300

    Criando estrutura do projeto

---
+++ src/test/java/com/cdaguiar/bookstoragemanager/BookstoragemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdaguiar.bookstoragemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoragemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
