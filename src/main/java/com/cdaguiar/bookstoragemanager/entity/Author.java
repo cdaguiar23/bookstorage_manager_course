962cc3a49c0dc05cf9aa842000354a8710b4e7ce
Claudionei de Aguiar <cdaguiar23@gmail.com>
2024-03-03 15:10:14 -0300

    Desenvolvendo entidades do projeto

---
+++ src/main/java/com/cdaguiar/bookstoragemanager/entity/Author.java
@@ -0,0 +1,28 @@
+package com.cdaguiar.bookstoragemanager.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private Integer age;
+
+}
